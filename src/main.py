3089448a398cf4e70b9f0422cd6ca7edd6916f56
Maya Vasu <maya.vasu.vrm@gmail.com>
2026-02-03 22:05:34 +0530

    home page setup

---
+++ src/main.py
@@ -0,0 +1,44 @@
+from nicegui import ui,app
+app.add_static_files('/img', 'img')
+
+def root():
+    # ui.sub_pages({
+    #     '/': home_page,
+    #     '/map/{lat}/{lon}': map_page, 
+
+    # }).classes('w-full')
+    home_page()
+
+def home_page():
+
+    with ui.column().classes('w-full gap-0 bg-no-repeat bg-cover bg-[url(/img/background.png)]').style('height: calc(100vh)'):
+        with ui.row().classes('w-full h-24'):
+            ui.image('/img/logo.png').classes('h-24 w-32')
+            ui.label('label 2')
+            ui.label('label 3')
+
+        with ui.row().classes('w-full h-40'):
+            ui.label('label 1')
+            ui.label('label 2')
+            ui.label('label 3')
+
+        with ui.row().classes('w-full grow'):
+            ui.label('label 1')
+            ui.label('label 2')
+            ui.label('label 3')
+
+def map_page(lat: float, lon: float):
+    ui.leaflet(center=(lat, lon), zoom=10)
+    ui.link('Back to table', '/')
+
+ui.add_css('''
+    :root {
+        --nicegui-default-padding: 0rem;
+        --nicegui-default-gap: 0rem;
+    }
+           
+
+''')
+
+home_page()
+ui.run(reload=True, host='127.0.0.1', port=6767)
