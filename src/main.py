dde06592f00e6559c4e8a27aa90ac73c349d3760
Maya Vasu <maya.vasu.vrm@gmail.com>
2026-02-04 12:04:27 +0530

    code change

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
-from nicegui import ui,app # type: ignore
+from nicegui import ui, app  # type: ignore
+
 app.add_static_files('/img', 'img')
 
-def home_page():
 
-    with ui.column().classes('w-full gap-0 bg-no-repeat bg-cover bg-[url(/img/background.png)]').style('height: calc(100vh)'):
+@ui.page('/')
+def home_page():
+    with ui.column().classes(
+        'w-full gap-0 bg-no-repeat bg-cover bg-[url(/img/background.png)]'
+    ).style('height: calc(100vh)'):
+
         with ui.row().classes('w-full h-24'):
             ui.image('/img/logo.png').classes('h-24 w-32')
             ui.label('')
@@ -19,18 +24,18 @@ def home_page():
             ui.label('label 2')
             ui.label('label 3')
 
+
+@ui.page('/map/{lat}/{lon}')
 def map_page(lat: float, lon: float):
     ui.leaflet(center=(lat, lon), zoom=10)
-    ui.link('Back to table', '/')
+    ui.link('Back to home', '/')
+
 
 ui.add_css('''
-    :root {
-        --nicegui-default-padding: 0rem;
-        --nicegui-default-gap: 0rem;
-    }
-           
-
+:root {
+    --nicegui-default-padding: 0rem;
+    --nicegui-default-gap: 0rem;
+}
 ''')
 
-home_page()
-ui.run(reload=True, host='127.0.0.1', port=6767)
+ui.run()
