99c5c2a37e6dbe49cf0e0ab8264ef5aeb78a2860
Maya Vasu <maya.vasu.vrm@gmail.com>
2026-02-04 22:49:30 +0530

    CodeChangeTopic home card update

---
+++ src/data.py
@@ -0,0 +1,574 @@
+topics = [
+    {
+        "topic": "Addition",
+        "description": "Add two-digit numbers with regrouping - sums to 100|Addition word problems - up to two digits|Add a multiple of 100 to a three-digit number|Addition with three-digit numbers",
+        "questions": [
+            {
+                "question": "Compare the numbers –1 ? –8. Pick the correct sign",
+                "choices": [
+                    "<", ">", "="
+                ],
+                "answer": ">",
+                "coins": 20
+            },
+            {
+                "question": '''
+                    Read this word problem.
+                    A gopher was digging 8 inches below ground. To avoid a tree root, it dug 3 inches deeper. What is the position of the gopher now relative to the surface?
+                    To solve the problem, Miles subtracted 
+                    –
+                    8–3 and came up with an answer of 
+                    –
+                    11 inches. Is Miles correct? Why or why not?
+                ''',
+                "choices": [
+                    "Yes, Miles is correct.",
+                    "No. The answer should have been negative 5 inches, not negative 11 inches, because the difference in the absolute values of the numbers is 5.",
+                    "No. Miles should have subtracted 8 − 3 because the gopher dug deeper."
+                ],
+                "answer": "Yes, Miles is correct.",
+                "coins": 50
+            },
+             {
+                "question":"Is –9 − 4 positive or negative?", 
+                "choices": [
+                   "positive","negative"
+                ],
+                "answer": "negative",
+                "coins": 10
+            },
+            {
+                "question":"Is 7 − 40 positive or negative?", 
+                "choices": [
+                   "positive","negative"
+                ],
+                "answer": "negative",
+                "coins": 10
+            },
+            {
+                "question":"Which property of multiplication is shown in (3 × 1) × 6 = 3 × (1 × 6)?", 
+                "choices": [
+                   "associative","commutative"
+                ],
+                "answer": "associative",
+                "coins": 25
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            
+        ]
+    },
+    {
+        "topic": "Algebra",
+        "description": "Evaluate variable expressions|Evaluate multi-variable expressions|Find the slope from a graph|Complete a table for a two-variable relationship|Inverse of a matrix",
+        "questions": [
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+        ]
+    },
+    {
+        "topic": "Comparing",
+        "description": "Fewer, more, and same|Put numbers up to 100 in order|Compare numbers up to one billion|Compare fractions using benchmarks|Compare decimal numbers",
+        "questions": [
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+
+        ]
+    },
+    {
+        "topic": "Counting",
+        "description": "Count dots - up to 3|Count out stickers - up to 10|Understand tens and ones - up to 30|Count groups of ten|Count forward and backward by fives and tens ",
+        "questions": [
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+        ]
+    },
+    {
+        "topic": "Decimals",
+        "description": "Put decimal numbers in order I|Multiply a decimal by a power of ten|Maps with decimal distances|Multiply and divide decimals by powers of ten",
+        "questions": [
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+    
+        ]
+    },
+    {
+        "topic": "Division",
+        "description": "Division facts to 12|Division facts to 12: word problems|Divide larger numbers by 1-digit numbers: interpret remainders|Divide numbers ending in zeros|Division with decimal quotients",
+        "questions": [
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+            {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+        ]
+    },
+    {
+        "topic": "Estimation",
+        "description": "Estimate differences: word problems|Estimate products: multiply by 2-digit numbers|Estimate sums, differences, products, and quotients: word problems|Estimate metric measurements",
+        "questions": [
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+        ]
+    },
+    {
+        "topic": "Exponents",
+        "description": "Evaluate powers with decimal bases|Evaluate powers with fractional bases|Positive and negative square roots|Simplify radical expressions with variables",
+        "questions": [
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+        ]
+    },
+    {
+        "topic": "Fractions",
+        "description": "Compare fractions|Identify equivalent fractions|Patterns of equivalent fractions|Write fractions in lowest terms|Simplify complex fractions",
+        "questions": [
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+             {
+                "question":"", 
+                "choices": [
+                   "","",""
+                ],
+                "answer": "",
+                "coins": 0
+            },
+
+        ]
+    }
+]
--- src/main.py
+++ src/main.py
@@ -1,31 +1,58 @@
-from nicegui import ui, app  #type: ignore
+from nicegui import ui, app
+from data import topics
+import random
 app.add_static_files('/img', 'img')
 
-@ui.page('/')
+
+
 def home_page():
     with ui.column().classes(
         'w-full gap-0 bg-no-repeat bg-cover bg-[url(/img/background.png)]'
     ).style('height: calc(100vh)'):
-        with ui.row().classes('w-full h-24'):
-            ui.image('/img/logo.png').classes('h-24 w-32')
-            ui.label('')
-            ui.label('label 3')
-        with ui.row().classes('w-full h-40'):
-            ui.label('label 1')
-            ui.label('label 2')
-            ui.label('label 3')
-        with ui.row().classes('w-full grow'):
-            ui.label('label 1')
-            ui.label('label 2')
-            ui.label('label 3')
-@ui.page('/map/{lat}/{lon}')
-def map_page(lat: float, lon: float):
-    ui.leaflet(center=(lat, lon), zoom=10)
-    ui.link('Back to home', '/')
+        with ui.row().classes('w-full h-24 items-center justify-between pr-10'):
+            ui.image('/img/logo.png').classes('h-28 w-36')
+            with ui.row().classes('items-center gap-4'):
+                ui.image('/img/avatar.png').classes('h-20 w-16 ')
+                ui.label('Maya').classes('inline-block align-middle text-white text-bold text-xl')
+            with ui.row().classes('items-center gap-4'):
+                ui.image('/img/coin.png').classes('h-18 w-18')
+                ui.label('100').classes('text-white text-bold inline-block align-middle text-xl')
+            with ui.row().classes('items-center gap-4'):
+                ui.button('MY LESSONS').classes('').props('color=white size=xl outline padding=xs').style('padding-left: 15px; padding-right: 15px;')
+                ui.button('MY STUFF').classes('').props('color=white size=xl outline padding=xs').style('padding-left: 15px; padding-right: 15px;')
+            with ui.row().classes('items-center gap-4'):
+                ui.button(icon='settings').classes('').props('q-btn round color="purple"')
+                ui.button(icon='close').classes('').props('q-btn round color="purple"')
+
+        with ui.row().classes('w-full h-40 justify-end pr-30 pt-10 pb-10'): 
+            with ui.column().classes('text-white bg-indigo-950 px-5 py-1 rounded-sm text-xl'):
+                with ui.row().classes('items-center'):
+                    ui.label('Weekly Goal').classes('mr-5')
+                    with ui.row().classes('gap-3'):
+                        for i in range(1,6):
+                                ui.row().classes('rounded-full border border-white h-4 w-4 ' )
+                with ui.row().classes('w-full justify-end'):
+                    ui.label('Lessons Completed This Week:  0')
+
+        with ui.row().classes('w-full grow pl-30 pr-30 pb-30'):
+            with ui.column().classes('bg-indigo-950 w-full h-full p-5 rounded-xl'):
+                ui.label('RECOMMENDATIONS').classes('text-xl text-bold text-white')
+                with ui.grid(columns=4).classes('w-full gap-5 h-full p-10'):
+                    for i in random.sample(topics, 8):
+                        
+                        with ui.row().classes('bg-purple w-full h-full rounded-2xl'):
+                            with ui.card().tight().classes('h-full'):
+                                with ui.card_section().classes('bg-purple w-full h-25'):
+                                    with ui.row().classes('items-center h-full w-full justify-center'):
+                                        ui.label(i['topic']).classes('text-3xl text-bold text-white')    
+                                with ui.card_section():
+                                    ui.label(i['description'])                   
 ui.add_css('''
 :root {
     --nicegui-default-padding: 0rem;
     --nicegui-default-gap: 0rem;
 }
 ''')
-ui.run()
+
+home_page()
+ui.run(host="127.0.0.1", port=6767, reload=True)
